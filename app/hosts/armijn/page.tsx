a3d114f45a5548df3676be7d880c3e39648e081e
Armijnv <armijnvandijk@gmail.com>
2026-04-30 17:57:34 -0300

    use proper og image

--- app/hosts/armijn/page.tsx
+++ app/hosts/armijn/page.tsx
@@ -10,12 +10,12 @@ export const metadata = {
     url: "https://homeinthe.city/hosts/armijn",
     siteName: "Home in the City",
     images: [
-      {
-        url: "/me.png",
-        width: 800,
-        height: 800,
-      },
-    ],
+  {
+    url: "/og-armijn2.jpg",
+    width: 1200,
+    height: 630,
+  },
+],
     locale: "en_US",
     type: "website",
   },
