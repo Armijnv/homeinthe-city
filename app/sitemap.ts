bef586fc4e8400fe4f80f71b2ae2445bf17d7227
Armijnv <armijnvandijk@gmail.com>
2026-05-01 17:11:52 -0300

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://homeinthe.city",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homeinthe.city/brazil/porto-alegre",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homeinthe.city/pt/brasil/porto-alegre",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homeinthe.city/hosts/armijn",
+      lastModified: new Date(),
+    },
+  ];
+}
