4d4d29cab2b6501ec6a16330de9a308ed268bdca
Armijnv <armijnvandijk@gmail.com>
2026-05-08 17:15:38 -0300

    Expand multilingual sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,78 @@
 export default function sitemap() {
   return [
+    /* ======================================================
+       HOME
+    ====================================================== */
+
     {
       url: "https://homeinthe.city",
       lastModified: new Date(),
     },
+
+    {
+      url: "https://homeinthe.city/pt",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://homeinthe.city/nl",
+      lastModified: new Date(),
+    },
+
+    /* ======================================================
+       CITY PAGES
+    ====================================================== */
+
     {
       url: "https://homeinthe.city/brazil/porto-alegre",
       lastModified: new Date(),
     },
+
     {
       url: "https://homeinthe.city/pt/brasil/porto-alegre",
       lastModified: new Date(),
     },
+
+    {
+      url: "https://homeinthe.city/nl/brazilie/porto-alegre",
+      lastModified: new Date(),
+    },
+
+    /* ======================================================
+       INTERPRETER PAGES
+    ====================================================== */
+
+    {
+      url: "https://homeinthe.city/interpreter-porto-alegre",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://homeinthe.city/pt/interprete-porto-alegre",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://homeinthe.city/nl/tolk-porto-alegre",
+      lastModified: new Date(),
+    },
+
+    /* ======================================================
+       HOST PAGE
+    ====================================================== */
+
     {
       url: "https://homeinthe.city/hosts/armijn",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://homeinthe.city/pt/hosts/armijn",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://homeinthe.city/nl/hosts/armijn",
       lastModified: new Date(),
     },
   ];
