a595a0be6a8c4e6bea1c0b86fa0819385cb04dd7
Armijnv <armijnvandijk@gmail.com>
2026-05-11 15:57:47 -0300

    Move Porto Alegre background to English route

--- app/brazil/porto-alegre/page.tsx
+++ app/brazil/porto-alegre/page.tsx
@@ -1,5 +1,12 @@
 import CityPage from "@/app/components/CityPage";
 
 export default function Page() {
-  return <CityPage lang="en" />;
+  return (
+    <>
+      <div className="pointer-events-none fixed inset-0 hidden bg-[url('/porto-alegre-desktop-background.jpg')] bg-cover bg-center md:block" />
+      <div className="pointer-events-none fixed inset-0 hidden bg-white/45 md:block" />
+
+      <CityPage lang="en" />
+    </>
+  );
 }
