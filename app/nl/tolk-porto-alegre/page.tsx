b70feb675a0b00185c445ce704df149be39d2548
Armijnv <armijnvandijk@gmail.com>
2026-05-11 14:42:08 -0300

    Add structured data to Dutch interpreter page

--- app/nl/tolk-porto-alegre/page.tsx
+++ app/nl/tolk-porto-alegre/page.tsx
@@ -38,5 +38,53 @@ export default async function Page() {
     slug: "interpreter-porto-alegre",
   });
 
-  return <InterpreterServicePage lang="nl" page={page} />;
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    name: "Home in the City - Nederlandse tolk in Porto Alegre",
+    description:
+      "Nederlandse tolk in Porto Alegre voor zakelijke meetings, fabrieksbezoeken en lokale begeleiding tijdens zakenreizen in Brazilië.",
+    url: "https://homeinthe.city/nl/tolk-porto-alegre",
+    areaServed: {
+      "@type": "City",
+      name: "Porto Alegre",
+      address: {
+        "@type": "PostalAddress",
+        addressLocality: "Porto Alegre",
+        addressRegion: "RS",
+        addressCountry: "BR",
+      },
+    },
+    serviceType: [
+      "Nederlandse tolk",
+      "Zakelijke tolk",
+      "Portugees Nederlands tolk",
+      "Begeleiding bij fabrieksbezoeken",
+      "Lokale ondersteuning tijdens zakenreizen",
+    ],
+    availableLanguage: ["Nederlands", "Engels", "Portugees"],
+    provider: {
+      "@type": "Person",
+      name: "Armijn van Dijk",
+      knowsLanguage: ["Nederlands", "Engels", "Portugees"],
+      jobTitle: "Interpreter and local business host in Porto Alegre",
+    },
+    contactPoint: {
+      "@type": "ContactPoint",
+      contactType: "customer service",
+      telephone: "+55 51 99778-3369",
+      availableLanguage: ["Nederlands", "Engels", "Portugees"],
+    },
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
+
+      <InterpreterServicePage lang="nl" page={page} />
+    </>
+  );
 }
