7eb25f0eafee8c802d00755b1fd906462f46e5d7
Armijnv <armijnvandijk@gmail.com>
2026-05-13 15:53:04 -0300

    Restore visible Portuguese Porto Alegre background

--- app/pt/brasil/porto-alegre/page.tsx
+++ app/pt/brasil/porto-alegre/page.tsx
@@ -36,9 +36,9 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="relative">
-      <div className="pointer-events-none fixed inset-0 -z-20 hidden bg-[url('/porto-alegre-desktop-background.jpg')] bg-cover bg-center md:block" />
-      <div className="pointer-events-none fixed inset-0 -z-10 hidden bg-white/25 md:block" />
+    <div className="relative isolate">
+      <div className="pointer-events-none fixed inset-0 z-0 hidden bg-[url('/porto-alegre-desktop-background.jpg')] bg-cover bg-center md:block" />
+      <div className="pointer-events-none fixed inset-0 z-0 hidden bg-white/25 md:block" />
 
       <CityPage lang="pt" />
     </div>
